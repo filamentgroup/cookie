a5d607d76b59109b046177049448daff1ea3dd26
Scott Jehl <scottjehl@gmail.com>
2014-07-10 10:36:30 -0500

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,27 +1,27 @@
 /*! cookie function. get, set, or forget a cookie. [c]2014 @scottjehl, Filament Group, Inc. Licensed MIT */
 function cookie( name, value, days ){
-		// if value is undefined, get the cookie value
-		if( value === undefined ){
-			var cookiestring = "; " + window.document.cookie;
-			var cookies = cookiestring.split( "; " + name + "=" );
-			if ( cookies.length == 2 ){
-				return cookies.pop().split( ";" ).shift();
-			}
-			return null;
+	// if value is undefined, get the cookie value
+	if( value === undefined ){
+		var cookiestring = "; " + window.document.cookie;
+		var cookies = cookiestring.split( "; " + name + "=" );
+		if ( cookies.length == 2 ){
+			return cookies.pop().split( ";" ).shift();
+		}
+		return null;
+	}
+	else {
+		// if value is a false boolean, we'll treat that as a delete
+		if( value === false ){
+			days = -1;
+		}
+		if ( days ) {
+			var date = new Date();
+			date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
+			var expires = "; expires="+date.toGMTString();
 		}
 		else {
-			// if value is a false boolean, we'll treat that as a delete
-			if( value === false ){
-				days = -1;
-			}
-			if ( days ) {
-				var date = new Date();
-				date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
-				var expires = "; expires="+date.toGMTString();
-			}
-			else {
-				var expires = "";
-			}
-			window.document.cookie = name+"="+value+expires+"; path=/";
+			var expires = "";
 		}
+		window.document.cookie = name + "=" + value + expires + "; path=/";
+	}
 }
